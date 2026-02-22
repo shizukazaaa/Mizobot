c2f0a9c4e8baba540545b32ad1e93f275b667d1c
shizukazaaa <shizukazaaa@gmail.com>
2026-02-22 11:18:11 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const app = express();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: "Bot พร้อมทำงานแล้ว"
+  });
+}
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running...');
+});
